093b28126d5eafdfa22bfe813760f148878da327
Christian Tischer <christian.tischer@embl.de>
2019-12-28 21:11:08 +0100

    Add four player example

--- src/main/java/tischi/economics/Main.java
+++ src/main/java/tischi/economics/Main.java
@@ -10,20 +10,33 @@ public class Main
 {
 	public static void main( String[] args )
 	{
+		final List<Player> players = initPlayers();
+
+		playGame(players);
+
+		displayResults(players);
+	}
+
+	public static List<Player> initPlayers() {
 		final List< Player > players = new ArrayList<>();
 		players.add( new Player( 100, "Bob" ));
 		players.add( new Player( 100, "Alice"));
+		players.add( new Player( 100, "Fred"));
+		players.add( new Player( 100, "Hans"));
+		return players;
+	}
 
+	public static void playGame(List<Player> players) {
 		final Game game = new Game( players, 0.2 );
 
-		final ArrayList<Integer> steps = Util.makeSteps(100);
-
-		for (int i = 1; i < steps.size(); i++) {
+		for (int i = 1; i < 1000; i++) {
 			game.step();
 		}
+	}
 
+	public static void displayResults(List<Player> players) {
 		// Create Chart
-		XYChart chart = new XYChartBuilder().width(600).height(500).title("Captial").xAxisTitle("T").yAxisTitle("C").build();
+		XYChart chart = new XYChartBuilder().width(1200).height(500).title("Captial").xAxisTitle("T").yAxisTitle("C").build();
 
 		// Customize Chart
 		chart.getStyler().setDefaultSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Line);
@@ -32,10 +45,19 @@ public class Main
 		//chart.getStyler().setMarkerSize(16);
 
 		for ( Player player : players ) {
-			chart.addSeries(player.getName(), steps, player.getHistory());
+			chart.addSeries(player.getName(), getTime(player), player.getHistory());
 		}
 
 		new SwingWrapper(chart).displayChart();
+	}
+
+	public static ArrayList<Integer> getTime(Player player) {
+		final ArrayList<Integer> time = new ArrayList<>();
+		final int size = player.getHistory().size();
+		for (int i = 0; i < size; i++) {
+			time.add( i );
+		}
+		return time;
 	}
 
 }
